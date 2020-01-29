71bca58191e779614a6bf015c1b33e1fe63d6adf
Kanzari <amine.kanzari@ett.tn>
2020-01-29 18:01:30 +0100

    Updates

---
+++ precache-manifest.3d8ab2c6acd508f28bd94e4e6237547d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863e4e5f3c659e57c2c73c1f8c37b3f6",
+    "url": "https//kanzariamine.github.io/readingbook/index.html"
+  },
+  {
+    "revision": "a27daf4d3fe3e7ebfb24",
+    "url": "https//kanzariamine.github.io/readingbook/static/css/main.f79cbd9c.chunk.css"
+  },
+  {
+    "revision": "bd668745a6044ac5eb0a",
+    "url": "https//kanzariamine.github.io/readingbook/static/js/2.6f892b36.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "https//kanzariamine.github.io/readingbook/static/js/2.6f892b36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a27daf4d3fe3e7ebfb24",
+    "url": "https//kanzariamine.github.io/readingbook/static/js/main.c3b0638e.chunk.js"
+  },
+  {
+    "revision": "b66a81a0b317876abde7",
+    "url": "https//kanzariamine.github.io/readingbook/static/js/runtime-main.9304c182.js"
+  }
+]);
